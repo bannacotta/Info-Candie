7d0a2d6ffda863e6bd39cf5397858a0221f9be0e
bannacotta <131975119+bannacotta@users.noreply.github.com>
2023-04-28 17:29:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ window.onscroll = () =>{
 	searchForm.classList.remove('active');
 	cartItem.classList.remove('active');
 } 
+
+let amountElement = document.getElementById('amount');
+let amount = amountElement.value;
+let minus = document.getElementById('minus');
+let plus = document.getElementById('plus');
+let render = (amount) => {
+	amountElement.value = amount;
+}
+
+minus.addEventListener('click', function(){
+	if(amount > 1){
+		amount--;
+		render(amount);
+	}
+	return(0);
+}); 
+
+plus.addEventListener('click', function(){
+	if(amount < 20){
+		amount++;
+		render(amount);
+	}
+});
+
+amountElement.addEventListener('input', function(){
+	amount = amountElement.value;
+	amount = parseInt (amount);
+	amount = (isNaN(amount)||amount == 0)?1:amount;
+	render(amount);
+});
